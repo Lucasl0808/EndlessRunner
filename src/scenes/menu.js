fdb930c2d1f6fbf3771b38e3929d8c8e27b4f8a8
Lucas Lee <luclee0808@gmail.com>
2023-05-11 20:34:19 -0700

    working on projectiles

--- src/scenes/menu.js
+++ src/scenes/menu.js
@@ -28,7 +28,7 @@ class menu extends Phaser.Scene {
 
         this.anims.create({
             key: "teleport",
-            frameRate: 20,
+            frameRate: 25,
             frames: this.anims.generateFrameNumbers("teleport", {start:0, end:2}),
         });
 
@@ -41,28 +41,29 @@ class menu extends Phaser.Scene {
 
         this.anims.create({
             key: "fireball",
-            frameRate: 4,
+            frameRate: 10,
             frames: this.anims.generateFrameNumbers("fireball", { start: 0, end: 3 }),
             repeat: -1
         });
 
         this.anims.create({
             key: "wave",
-            frameRate: 4,
+            frameRate: 2,
             frames: this.anims.generateFrameNumbers("wave", { start: 0, end: 2 }),
         });
 
-        this.arrow = this.add.sprite(600, 250, 'arrow');
+        this.arrow = this.add.sprite(600, 300, 'arrow');
         this.arrow.play('arrow');
 
         this.run = this.add.sprite(100, 120,'runner');
         this.run.play('running');
 
-        this.fire = this.add.sprite(600, 450, 'fireball');
+        this.fire = this.add.sprite(600, 500, 'fireball');
         this.fire.play('fireball');
 
         this.wave = this.add.sprite(600, 90, 'wave');
         this.wave.play('wave');
+
     }
     update(){
         if(Phaser.Input.Keyboard.JustDown(keyUP) && (this.run.y - 200) > 0){
@@ -86,6 +87,39 @@ class menu extends Phaser.Scene {
             });
         }
         this.bg.tilePositionX += 2;
+
+        //1, 4, 11 = wave
+        //2,5, 10 = arrow
+        //3, 6, 12 = fireball
+        let proj1 = Phaser.Math.Between(1,3);
+        let proj2;
+        if(proj1 == 1){
+            proj2 == Phaser.Math.Between(5,6); // if wave, choose either fire or arrow
+            if(proj2 == 5){
+                //wave and arrow - call prefab functions to spawn them in
+            }
+            else{
+                //wave and fire
+            }
+        }
+        if(proj1 == 2){
+            proj2 == Phaser.Math.Between(11,12);//if arrow, choose either fire or wave
+            if(proj2 == 11){
+                //arrow and wave
+            }
+            else{
+                //arrow and fire
+            }
+        }
+        if(proj1 == 3){
+            proj2 == Phaser.Math.Between(4,5);//if fire, choose either arrow or wave
+            if(proj2 == 4){
+                //fire and wave
+            }
+            else{
+                //fire and arrow
+            }
+        }
     }
 
 
