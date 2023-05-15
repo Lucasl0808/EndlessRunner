997bebe165b993e1be6b3b6ecb9edf2485e4931d
Lucas Lee <luclee0808@gmail.com>
2023-05-15 15:35:47 -0700

    labeled credit

--- src/scenes/credits.js
+++ src/scenes/credits.js
@@ -4,7 +4,7 @@ class creditScene extends Phaser.Scene {
     }
     create(){
         this.add.text(centerx-100, centery-200, 'Credits!', titleConfig);
-        this.add.text(centerx-100, centery - 100, 'https://www.youtube.com/watch?v=3nQNiWdeH2Q', textConfig);
+        this.add.text(centerx-100, centery - 100, 'bgm - https://www.youtube.com/watch?v=3nQNiWdeH2Q', textConfig);
         this.add.text(centerx-100, centery - 50, 'Press the M key to go back to Menu', textConfig);
         keyM = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.M);
 
