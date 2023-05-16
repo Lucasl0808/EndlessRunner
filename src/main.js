f44547f69f508d2f1860d1f4b47be526c24e03d2
Lucas Lee <luclee0808@gmail.com>
2023-05-15 23:18:01 -0700

    added comment block in main.js

--- src/main.js
+++ src/main.js
@@ -1,3 +1,18 @@
+/*
+Name: Lucas Lee
+Game title: Portal Jumpers
+Time Spent: 25 Hours
+
+For my technically interesting thing, I tried to make a system where projectiles would be fired randomly and each one of them being a different kind of projectile.
+This was somewhat similar to the paddleParkour, but I made sure to create a different projectile and use the Math.between() function in a different way in order to
+determine which projectile would be fired.
+
+My visual style is unique in the sense that I drew all of the art assets on my own (this was a lot of work since I did it on a trackpad :( ) I'm particularly proud of the fireball animation
+that I drew since it was hard to draw up that idea from scratch. I tried to create a project that reflected an idea that I had where a player would be able to jump to different 
+"scenes" or areas using teleportation and somehow came up with this theme of teleporting to dodge projectiles from 3 different places. I had a fun time making a difficulty scale
+using color schemes and text at the top of the game, as it gets harder, the game will let you know that you've made it to a harder part of the game!
+*/
+
 let config = {
     type: Phaser.AUTO,
     width: 700,
